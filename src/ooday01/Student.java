d28f0f0625158c1366c6739305406f8d4f932a27
tedu <shinoyashirosama@gmail.com>
2024-03-15 14:21:02 +0800

    更新0

--- src/ooday01/Student.java
+++ src/ooday01/Student.java
@@ -1,13 +1,24 @@
 package ooday01;
-
+//构造函数，构造器，构建器
 public class Student {
-    String name;
+    String name;//成员变量（整个类中生效
     String classname;
     int stuID;
     int age;
+    Student(){
+    }
+    //不写构造会默认有个无参构造 Student(){}
+    // 构造方法
+    Student(String name,String classname,int stuID,int age){
+        this.name = name;//this可以访问成员变量。------当成员变量域局变量同名时不能省略this
+        this.classname = classname;
+        this.stuID = stuID;
+        this.age = age;
+    }
 
     void sayHi(){
         System.out.println("大家好，我叫"+this.name+"我的学号是"+this.stuID+"我的班级是"+this.classname+"我今年"+this.age+"岁了");
+        //此处可以省略this，因为构造方法中的name离开局部失效，此处只能读到成员变量
     }
     void playwith(String another){
         System.out.println(this.name+"正在和"+another+"玩");
--- src/practicedraft/studypoint.java
+++ src/practicedraft/studypoint.java
@@ -1,8 +1,17 @@
 package practicedraft;
 
+import ooday01.Student;
+
 public class studypoint {
     public static void main(String[] args) {
-        /*问题集：JVM是什么，如何进行JVM调优，调优策略
+        //问题集：JVM是什么，如何进行JVM调优，调优策略
+        //Student knt = new Student("Konata","JSD2403",01,23);
+//        Student Konata = new Student();
+//        Konata.name = "Konata";
+//
+//        Konata.sayHi();//String 默认值为null,int 为 0；
+//        Konata.playwith("Kagami");
 
-        */}
+        }
+
 }
