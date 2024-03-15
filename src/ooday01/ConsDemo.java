b159bee7b8f10a6929c6feb1f95f671d08807e3e
tedu <shinoyashirosama@gmail.com>
2024-03-15 14:21:02 +0800

    更新0

---
+++ src/ooday01/ConsDemo.java
@@ -0,0 +1,10 @@
+package ooday01;
+
+public class ConsDemo {
+    public static void main(String[] args) {
+        Student knt = new Student("Konata","JSD2403",01,23);
+        knt.sayHi();
+        knt.playwith("kagami");
+
+    }
+}
--- src/ooday01/Student.java
+++ src/ooday01/Student.java
@@ -5,6 +5,13 @@ public class Student {
     String classname;
     int stuID;
     int age;
+    // 构造方法(重构)
+    Student(String name1,String classname1,int stuID1,int age1){
+        this.name = name1;
+        this.classname = classname1;
+        this.stuID = stuID1;
+        this.age = age1;
+    }
 
     void sayHi(){
         System.out.println("大家好，我叫"+this.name+"我的学号是"+this.stuID+"我的班级是"+this.classname+"我今年"+this.age+"岁了");
