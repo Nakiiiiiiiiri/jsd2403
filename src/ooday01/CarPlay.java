529290d0881062281946c1e4d4a44169896384e7
tedu <shinoyashirosama@gmail.com>
2024-03-15 17:58:14 +0800

    更新0

--- src/ooday01/CarPlay.java
+++ src/ooday01/CarPlay.java
@@ -2,10 +2,23 @@ package ooday01;
 
 public class CarPlay {
     public static void main(String[] args) {
-        Car hd = new Car("比亚迪",79800.0 ,"白色");
+        Car ms = new Car();
+        ms.brand = "三菱";
+        ms.price = 298000.0;
+        ms.color = "白色";
+
+
+        Car byd = new Car("比亚迪",79800.0 ,"白色");
+        Car hd = new Car("本田",13000.0 ,"黄色");
+        byd.start();
+        byd.run();
+        byd.stop();
         hd.start();
         hd.run();
         hd.stop();
+        ms.start();
+        ms.run();
+        ms.stop();
 
     }
 }
